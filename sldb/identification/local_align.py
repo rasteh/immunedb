28ba788332991008a1f02ee1b88bcf391f17fd82
Aaron Rosenfeld <ar374@drexel.edu>
2016-01-07 16:26:10 -0500

    messy testing code

--- sldb/identification/local_align.py
+++ sldb/identification/local_align.py
@@ -4,6 +4,7 @@ import subprocess
 import shlex
 from sqlalchemy import func
 
+import dnautils
 import sldb.util.funcs as funcs
 from sldb.identification import AlignmentException
 from sldb.identification.j_genes import JGermlines
@@ -27,113 +28,145 @@ def gap_positions(seq):
 
 
 class LocalAlignmentWorker(concurrent.Worker):
-    def __init__(self, local_align_path, max_deletions, max_insertions):
-        self._local_align_path = local_align_path
+    def __init__(self, align_path, max_deletions, max_insertions):
+        self._align_path = align_path
         self._max_deletions = max_deletions
         self._max_insertions = max_insertions
 
     def do_task(self, args):
-        j_align = self._align_seq_to_germs(
-            args['seq_id'], args['seq'], args['j_ties']
-        )
-
-        if j_align is None:
-            print args['seq_id'], 'Bad J'
-            return
-        v_seq = args['seq'][:j_align['seq_pos']]
-        v_align = self._align_seq_to_germs(args['seq_id'], v_seq,
-                                           args['v_ties'])
+        # Find best aligned first allele
+        first_alleles = {
+            name: v.sequence for name, v in args['vs'].iteritems()
+            if int(name.split('*', 1)[1]) == 1
+        }
+        v_align = self._align_seq_to_germs(args['seq_id'], args['seq'],
+                                           first_alleles)
         if v_align is None:
             print args['seq_id'], 'Bad V'
             return
 
-        missing_start = v_align['seq_pos'] + v_align['seq_len']
-        missing_len = (j_align['seq_pos'] - v_align['seq_pos']
-            - len(v_align['seq']))
-        missing = args['seq'][missing_start:missing_start + missing_len]
+        v_name = v_align['germ_name'].split('*', 1)[0]
+        v_ties = {
+            '|'.join(name): v for name, v in args['vs'].all_ties(
+                args['avg_len'], args['avg_mut'], cutoff=False
+            ).iteritems() if v_name in '|'.join(name)
+        }
+        v_align = self._align_seq_to_germs(
+            args['seq_id'], v_align['seq'].replace('-', ''), v_ties
+        )
 
-        new_seq = ''.join((
-            v_align['seq'],
-            missing,
-            j_align['seq']
-        ))
-
-        v_stripped = v_align['germ'].replace('-', '')
-        missing = v_align['germ_orig'][
-            v_align['germ_pos'] + len(v_stripped):
-            v_align['germ_pos'] + len(v_stripped) + missing_len
-        ].lower()
-        new_germ = ''.join((
-            v_align['germ'],
-            missing,
-            j_align['germ']
-        ))
-
-        if v_align['germ_pos'] > 0:
-            new_germ = v_align['germ_orig'][:v_align['germ_pos']] + new_germ
-            diff = len(new_germ) - len(new_seq)
-            if diff > 0:
-                new_seq = ''.join((
-                    v_align['seq_orig'][:v_align['seq_pos']][-diff:],
-                    new_seq
-                ))
-        #TODO: Add back germline J if sequence is partial
-
-	gapped_germ = args['v_ties'][tuple(v_align['germ_name'].split('|'))]
-        germ_gaps = gap_positions(v_align['germ'])
+        germ_insertions = gap_positions(v_align['germ'])
         imgt_gaps = [
-            i + gaps_before(germ_gaps, i)
-            for i, c in enumerate(gapped_germ) if c == '-'
+            i + gaps_before(germ_insertions, i)
+            for i, c in enumerate(v_ties[v_align['germ_name']]) if c == '-'
         ]
         for gap in imgt_gaps:
-            new_germ = new_germ[:gap] + '-' + new_germ[gap:]
-            new_seq = new_seq[:gap] + '-' + new_seq[gap:]
+            v_align['germ'] = ''.join((
+                v_align['germ'][:gap],
+                '^',
+                v_align['germ'][gap:]
+            ))
+            v_align['seq'] = ''.join((
+                v_align['seq'][:gap],
+                '-',
+                v_align['seq'][gap:]
+            ))
 
-        cdr_start = 309 + v_align['germ'].count('-')
-        cdr_end = (
-            len(new_seq) - args['j_ties'].upstream_of_cdr3 -
-            j_align['germ'].count('-')
+        cdr3_start = 0
+        count = 0
+        for i, c in enumerate(v_align['germ']):
+            if c != '-':
+                count += 1
+                if count > 309:
+                    cdr3_start = i
+                    break
+        v_align['germ'] = v_align['germ'].replace('^', '-')
+        print v_ties[v_align['germ_name']]
+        print v_align['germ_name']
+        print v_align['germ']
+        print (' ' * cdr3_start) + '|'
+        print v_align['seq']
+
+        j_align = self._align_seq_to_germs(
+            args['seq_id'], v_align['seq'][cdr3_start:], args['js']
         )
+        print j_align['germ_name']
+        print j_align['germ']
+        print j_align['seq']
+
+        if j_align is None:
+            print args['seq_id'], 'Bad J'
+            return
+
+        final_germ = ''.join((
+            v_align['germ'][:-len(j_align['germ'])],
+            j_align['germ']
+        ))
+        final_seq = ''.join((
+            v_align['seq'][:-len(j_align['seq'])],
+            j_align['seq']
+        ))
+        final_germ = final_germ.rstrip('-')
+        final_seq = final_seq[:len(final_germ)]
+        cdr3_end = len(final_seq) - args['js'].upstream_of_cdr3
+        final_germ = ''.join((
+            final_germ[:cdr3_start],
+            '-' * (cdr3_end - cdr3_start),
+            final_germ[cdr3_end:]
+        ))
+
+        print 'final!'
+        print final_germ
+        print (' ' * cdr3_start) + '|'
+        print (' ' * cdr3_end) + '|'
+        print final_seq
+
+        '''
+        final_seq = ''.join((
+            v_align['germ'][:cdr3_start],
+            j_align['germ']
+        ))
+        print args['seq_id'], 'good', v_align['germ_name']
+        '''
 
     def _align_seq_to_germs(self, seq_id, seq, germs):
         stdin = []
         for g_name, g_seq in germs.iteritems():
-            if isinstance(g_name, tuple):
-                g_name = '|'.join(g_name)
             stdin.append('>{}\n{}\n'.format(g_name, g_seq.replace('-', '')))
             stdin.append('>{}\n{}\n'.format(seq_id, seq))
 
         cmd = (
-            '{} --match 4 --mismatch -2 --gapopen -5 --gapextend -2 '
-            '--wildcard N 2 --maxhits 1 --file - '
-            '--printfasta --printseq'
-        ).format(self._local_align_path)
+            '{} --match 4 --mismatch -3 --gapopen -5 --gapextend -2 '
+            '--wildcard N 4 --printfasta --printscores --freestartgap '
+            '--freeendgap --file -'
+        ).format(self._align_path)
         proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         output, err = proc.communicate(''.join(stdin))
         regex = (
-            r'==[^\n]+\n'
             r'(?P<germ_name>.+)\n'
-            r'(?P<germ_orig>.+)\n'
-            r'(?P<seq_name>.+)\n'
-            r'(?P<seq_orig>.+)\n\n'
-            r'hit .* (?P<score>.+)\n'
-            r'\s*(?P<germ>[^\s]+)\s*'
-            r'\[pos: (?P<germ_pos>\d+); len: (?P<germ_len>\d+)\]\n'
-            r'\s*(?P<seq>[^\s]+)\s*'
-            r'\[pos: (?P<seq_pos>\d+); len: (?P<seq_len>\d+)\]'
+            r'(?P<germ_padding>[-]*)(?P<germ>[ATCGN-]+)\n'
+            r'.+\n'
+            r'(?P<seq_padding>[-]*)(?P<seq>[ATCGN-]+)\n'
+            r'score: (?P<score>\d+)\n'
         )
 
         best = None
         for match in re.finditer(regex, output, re.MULTILINE):
             match = match.groupdict()
+            match['germ_offset'] = len(match['germ_padding'])
+            match['seq_offset'] = len(match['seq_padding'])
             match['score'] = int(match['score'])
-            match['germ_pos'] = int(match['germ_pos'])
-            match['germ_len'] = int(match['germ_len'])
-            match['seq_pos'] = int(match['seq_pos'])
-            match['seq_len'] = int(match['seq_len'])
             if best is None or match['score'] > best['score']:
                 best = match
+
+        if best is None:
+            return None
+        if best['germ_offset'] > 0:
+            best['seq'] = best['seq'][best['germ_offset']:]
+        elif best['seq_offset'] > 0:
+            best['seq'] = ('N' * best['seq_offset']) + best['seq']
+
         return best
 
 
@@ -144,7 +177,9 @@ def run_fix_sequences(session, args):
     mutation_cache = {}
     tasks = concurrent.TaskQueue()
 
-    #indels = session.query(NoResult).limit(100)
+    '''
+    indels = session.query(NoResult).limit(1000)
+    '''
     indels = session.query(NoResult).filter(
         NoResult.seq_id == 'M03592:1:000000000-ADANF:1:2119:10104:6789'
     )
@@ -163,8 +198,8 @@ def run_fix_sequences(session, args):
             'sample_id': seq.sample_id,
             'seq_id': seq.seq_id,
             'seq': seq.sequence,
-            'v_ties': v_germlines.all_ties(avg_len, avg_mut, cutoff=False),
-            'j_ties': j_germlines,
+            'vs': v_germlines,
+            'js': j_germlines,
             'avg_mut': avg_mut,
             'avg_len': avg_len
         })
@@ -173,7 +208,7 @@ def run_fix_sequences(session, args):
     workers = min(args.nproc, tasks.num_tasks)
     for i in range(0, workers):
         tasks.add_worker(LocalAlignmentWorker(
-            args.local_align_path,
+            args.align_path,
             args.max_deletions,
             args.max_insertions)
         )
